f695114cfa8681875b110bca7a1cf48c58d5e6af
Unknown <skyesladeut@outlook.com>
2019-05-14 21:26:42 -0600

    Fixing Movement Again...

--- Unity/SummerGameScripts/Assets/Scripts/Movement_Scripts/Character_Movement_JoystickConf.cs
+++ Unity/SummerGameScripts/Assets/Scripts/Movement_Scripts/Character_Movement_JoystickConf.cs
@@ -10,9 +10,7 @@ public class Character_Movement_JoystickConf : MonoBehaviour
     Transform MainCamera;
     public UnityEvent Reach_Destination;
     public Player_Object player;
-    public Joystick_Move_Pattern Automatic_Forward;
     private CharacterController _controller;
-    private Vector3 _destination, _direction, offset;
     private Quaternion _rotation;
     private float angle, offsetAngle;
     public float RotationFloat, SpeedFloat;
@@ -26,7 +24,6 @@ public class Character_Movement_JoystickConf : MonoBehaviour
     {
         
         CRRunning = false;
-        offset.Set(.1f, .1f, .1f);
         _controller = GetComponent<CharacterController> ();
         MainCamera = Camera.main.transform;
         EnableCC();
@@ -60,147 +57,71 @@ public class Character_Movement_JoystickConf : MonoBehaviour
         StartCoroutine(Walk_Backward());
     }
 
-    private IEnumerator Walk_Towards()
-    {
-        Debug.Log("Walk");
-        ReachedDestination.value = false;
-        angle = 180;
-        _destination = target.trans.position;
-        _destination.y = transform.position.y;
-        _rotation = target.trans.rotation;
-        _direction = _destination - transform.position;
-        CRRunning = true;
-        StartCoroutine(TurnAround(RotationFloat, SpeedFloat, false));
-        yield return new WaitUntil(() => CRRunning == false);
-        CRRunning = true;
-        StartCoroutine(Walk(0, SpeedFloat, _destination));
-        yield return new WaitUntil(() => CRRunning == false);
-        CRRunning = true;
-        StartCoroutine(ReOrient(RotationFloat, 0, _rotation.y, false));
-        yield return new WaitUntil(() => CRRunning == false);
-        print("Done");
-        Reach_Destination.Invoke();
-    }
 
-    private IEnumerator Walk_Backward()
-    {
-        Debug.Log("Walk_Back");
-        ReachedDestination.value = false;
-        angle = 180;
-        _destination = target.trans.position;
-        _destination.y = transform.position.y;
-        _rotation = target.trans.rotation;
-        _direction = _destination - transform.position;
-        CRRunning = true;
-        StartCoroutine(TurnAround(RotationFloat, -SpeedFloat, true));
-        yield return new WaitUntil(() => CRRunning == false);
-        CRRunning = true;
-        StartCoroutine(Walk(0, -SpeedFloat, _destination));
-        yield return new WaitUntil(() => CRRunning == false);
-        CRRunning = true;
-        StartCoroutine(ReOrient(RotationFloat, 0, _rotation.y, true));
-        yield return new WaitUntil(() => CRRunning == false);
-        print("Done");
-        yield return new WaitForSeconds(.1f);
-        SetPosition();
-        Reach_Destination.Invoke();
-    }
-
-    public IEnumerator TurnAround(float turnspeed, float walkspeed, bool backwards)
-    {
-        Debug.Log("Turn Around");
-        CRRunning = true;
-        Automatic_Forward.TurnSpeed.value = turnspeed;
-        Automatic_Forward.MoveSpeed.value = walkspeed;
-        angle = Vector3.Angle(_direction, transform.forward);
-        Debug.Log(angle);
-        while (((angle >= 10 && !backwards) || (angle <= 170 && backwards)) && !ReachedDestination.value)
-        {
-            _direction = _destination - transform.position;
-            if (backwards)
-                angle = Vector3.Angle(_direction, -transform.forward);
-            else
-                angle = Vector3.Angle(_direction, transform.forward);
-            Automatic_Forward.AutomaticMove(transform, _controller, MainCamera, angle);
-            Debug.Log(angle);
-            yield return new WaitForFixedUpdate();
-        }
-        Automatic_Forward.TurnSpeed.value = turnspeed/2;
-        Automatic_Forward.MoveSpeed.value = walkspeed/2;
-        while (((angle >= 2 && !backwards) || (angle <= 178 && backwards)) && !ReachedDestination.value)
-        {
-            _direction = _destination - transform.position;
-            if (backwards)
-                angle = Vector3.Angle(_direction, -transform.forward);
-            else
-                angle = Vector3.Angle(_direction, transform.forward);
-            Automatic_Forward.AutomaticMove(transform, _controller, MainCamera, angle);
-            Debug.Log(angle);
-            yield return new WaitForFixedUpdate();
-        }
-        print("Done");
-        CRRunning = false;
-    }
-
-    public IEnumerator Walk(float turnspeed, float walkspeed, Vector3 destination)
-    {
-        Debug.Log("Walk");
-        CRRunning = true;
-        Automatic_Forward.TurnSpeed.value = turnspeed;
-        Automatic_Forward.MoveSpeed.value = walkspeed;
-        angle = Vector3.Angle(_direction, transform.forward);
-        while (!ReachedDestination.value)
-        {
-            Automatic_Forward.TurnSpeed.value = 0;
-            Automatic_Forward.AutomaticMove(transform, _controller, MainCamera, angle);
-            yield return new WaitForFixedUpdate();
-            if (((transform.position.z <= (_destination + offset).z) &&
-                 (transform.position.x <= (_destination + offset).x))
-                && ((transform.position.z >= (_destination - offset).z) &&
-                    (transform.position.x >= (_destination - offset).x)))
-                break;
-        }
-
-        CRRunning = false;
-    }
-
-    public IEnumerator ReOrient(float turnspeed, float walkspeed, float rotation, bool backwards)
-    {
-        CRRunning = true;
-        Automatic_Forward.TurnSpeed.value = turnspeed;
-        Automatic_Forward.MoveSpeed.value = walkspeed;
-        angle = Vector3.Angle(_direction, transform.forward);
-        while ((transform.rotation.y > rotation + .05f || transform.rotation.y < rotation -.05f)&& !ReachedDestination.value)
-        {
-            _direction = _destination - transform.position;
-            if (backwards)
-                angle = Vector3.Angle(_direction, -transform.forward);
-            else
-                angle = Vector3.Angle(_direction, transform.forward);
-            Automatic_Forward.AutomaticMove(transform, _controller, MainCamera, angle);
-            yield return new WaitForFixedUpdate();
-        }
-
-        CRRunning = false;
-    }
 
     public void SetPosition()
     {
-        transform.position = _destination;
-        transform.rotation = _rotation;
-    }
-    
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Puzzle"))
-        {
-            target.trans = other.transform.Find("ViewingSpot");
-        }
+        transform.position = target.trans.position;
+        transform.rotation = target.trans.rotation;
     }
 
     public void LoadDest()
     {
         transform.position = Load_Destination.vector;
+    }
+
+    public IEnumerator Walk_Towards()
+    {
+        _rotation = target.trans.rotation;
+        _rotation.y += 180;
+        ReachedDestination.value = false;
+        CRRunning = true;
+        StartCoroutine(Rotate());
+        yield return new WaitUntil( () => CRRunning == false);
+        StartCoroutine(Walk());
+        yield return new WaitUntil( () => CRRunning == false);
+        Reach_Destination.Invoke();
+    }
+    
+    
+    public IEnumerator Walk_Backward()
+    {
+        ReachedDestination.value = false;
+        _rotation = target.trans.rotation;
+        CRRunning = true;
+        StartCoroutine(Rotate());
+        yield return new WaitUntil( () => CRRunning == false);
+        StartCoroutine(Walk());
+        yield return new WaitUntil( () => CRRunning == false);
+        Reach_Destination.Invoke();
+    }
+
+    public IEnumerator Rotate()
+    {
+        Debug.Log("Rotate");
+        CRRunning = true;
+        while ((transform.rotation.y >= _rotation.y + 1f) || (transform.rotation.y <= _rotation.y - 1f))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, _rotation, RotationFloat * Time.deltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+        Debug.Log("Rotate Done");
+        CRRunning = false;
+    }
+    
+
+    public IEnumerator Walk()
+    {
+        Debug.Log("Walk");
+        CRRunning = true;
+        while (!ReachedDestination.value)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.trans.position, SpeedFloat * Time.deltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+        Debug.Log("Walk Done");
+        SetPosition();
+        CRRunning = false;
     }
     
     
